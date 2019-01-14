84ee9e8761e5534ebd11f11d2c10939c80aad2ca
Daisuke Akimoto <daisuke@laurahouse.net>
2019-01-14 09:05:24 +0900

    First commit.

---
+++ App.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import { FlatList } from "react-native";
+import { Container, Header, Left, Body, Right, Title, Content, List, ListItem, Text } from 'native-base';
+import Realm from 'realm'
+
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { realm: null };
+  }
+
+  componentWillMount() {
+    Realm.open({
+      schema: [{name: 'Person', properties: {key: 'string', name: 'string'}}]
+    }).then(realm => {
+      realm.write(() => {
+        var current = new Date();
+        realm.create('Person', {key: current.toString(), name: 'daisuke'});
+      });
+      console.log(realm);
+      this.setState({ realm });
+    });
+  }
+
+
+  render() {
+    const displayItem = this.state.realm ? this.state.realm.objects('Person') : [] 
+
+    return (
+      <Container>
+        <Header>
+          <Left />
+          <Body>
+            <Title>Realmサンプル</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+        <FlatList
+          data={displayItem}
+          renderItem={({item}) =>
+            <ListItem>
+              <Text>{item.name}</Text>
+            </ListItem>
+          }
+          keyExtractor={item => item.key}
+        />
+        </Content>
+      </Container>
+    );
+  }
+}
